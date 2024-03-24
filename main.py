bbfd675a3ccdeb731d035b4996d62ff81776832b
RatanPrakash <ratanprakash892@gmail.com>
2024-03-24 16:39:18 +0530

    deleted unnecessary files

--- main.py
+++
@@ -1,5 +0,0 @@
-# This file is the entry point of the program. It calls the interview function from the interview module.
-from interview import interview
-
-if __name__ == '__main__':
-    interview()
